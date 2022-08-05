5343d988d216ed6a7a9b032d027f94bcbd41c2fd
JEGP2712 <jorgegarcia143@gmail.com>
2022-08-05 11:15:21 -0300

    Dentro de la carpeta public creo 2 carpetas (css,images). Comienzo a editar el html de ProductDetail

--- Digital-Delux/app.js
+++ Digital-Delux/app.js
@@ -8,6 +8,10 @@ app.get('/', function(req, res){
     res.sendFile(path.join(__dirname, '/views/index.html'))
 })
 
+app.get('/product', function(req, res){
+    res.sendFile(path.join(__dirname, '/views/productDetail.html'))
+})
+
 app.listen(3000, ()=>{
     console.log('Servidor corriendo en el puerto 3000');
 })
