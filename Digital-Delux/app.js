e0ffc7b050bf17d2745fa7bef58e60d7fbc2101c
FranciscoJNavas <navas.francisco.j@gmail.com>
2022-08-06 17:31:21 -0300

    Completando index.html y agregando ícono en navegador

--- Digital-Delux/app.js
+++ Digital-Delux/app.js
@@ -12,6 +12,22 @@ app.get('/product', function(req, res){
     res.sendFile(path.join(__dirname, '/views/productDetail.html'))
 })
 
+app.get('/register', function(req, res){
+    res.sendFile(path.join(__dirname, '/views/register.html'))
+})
+
+app.get('/login', function(req, res){
+    res.sendFile(path.join(__dirname, '/views/login.html'))
+})
+
 app.listen(3000, ()=>{
     console.log('Servidor corriendo en el puerto 3000');
 })
+
+app.post('/register', (req,res)=>{
+    res.sendFile(__dirname + '/views/index.html');
+});
+
+app.post('/login', (req,res)=>{
+    res.sendFile(__dirname + '/views/index.html');
+});
